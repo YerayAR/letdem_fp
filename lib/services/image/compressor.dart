13e6420cda76a46c12008b6fa8cd1e25a3775f30
Vehan Hemsara <79366878+Hemsara@users.noreply.github.com>
2025-02-19 21:38:33 +0000

    feat: add image compression functionality; implement ImageCompressor class and update dependencies

---
+++ lib/services/image/compressor.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:image/image.dart' as img;
+
+class CompressionError extends Error {
+  final String message;
+
+  CompressionError(this.message);
+
+  @override
+  String toString() {
+    return message;
+  }
+}
+
+class ImageCompressor {
+  static Future<String> compressImageToBase64(File imageFile,
+      {int quality = 50}) async {
+    try {
+      Uint8List imageBytes = await imageFile.readAsBytes();
+
+      img.Image? image = img.decodeImage(imageBytes);
+      if (image == null) {
+        throw Exception("Failed to decode image.");
+      }
+
+      image =
+          img.copyResize(image, width: 800); // Resize to 800px width (optional)
+
+      List<int> compressedBytes = img.encodeJpg(image, quality: quality);
+
+      String base64String = base64Encode(compressedBytes);
+
+      return base64String;
+    } catch (e) {
+      throw CompressionError("Failed to compress image: $e");
+    }
+  }
+}
