43f72cd0aeecdee50d543b593c844796449c32c5
Vehan Hemsara <79366878+Hemsara@users.noreply.github.com>
2025-03-08 21:50:15 +0000

    feat: add image picker functionality to publish space; enhance UI interaction for event publishing

--- lib/views/app/activities/widgets/no_contribution.widget.dart
+++ lib/views/app/activities/widgets/no_contribution.widget.dart
@@ -1,9 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:letdem/constants/ui/colors.dart';
 import 'package:letdem/constants/ui/dimens.dart';
 import 'package:letdem/constants/ui/typo.dart';
+import 'package:letdem/global/popups/popup.dart';
 import 'package:letdem/global/widgets/button.dart';
+import 'package:letdem/views/app/home/widgets/home/bottom_sheet/add_event_sheet.widget.dart';
+import 'package:letdem/views/app/publish_space/screens/publish_space.view.dart';
 
 class NoContributionsWidget extends StatelessWidget {
   const NoContributionsWidget({super.key});
@@ -30,21 +36,40 @@ class NoContributionsWidget extends StatelessWidget {
         SizedBox(
           width: MediaQuery.of(context).size.width / 1.9,
           child: PrimaryButton(
-            onTap: () {},
+            onTap: () async {
+              ImagePicker picker = ImagePicker();
+              var pickedFile =
+                  await picker.pickImage(source: ImageSource.camera);
+
+              if (pickedFile == null) return;
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      PublishSpaceScreen(file: File(pickedFile!.path)),
+                ),
+              );
+            },
             icon: Iconsax.location5,
             text: 'Publish Space',
           ),
         ),
         Dimens.space(1),
-        Center(
-            child: Text(
-          "Publish an event",
-          style: Typo.mediumBody.copyWith(
-            color: AppColors.primary400,
-            fontWeight: FontWeight.w500,
-            decoration: TextDecoration.underline,
-          ),
-        )),
+        GestureDetector(
+          onTap: () {
+            AppPopup.showBottomSheet(context, const AddEventBottomSheet());
+          },
+          child: Center(
+              child: Text(
+            "Publish an event",
+            style: Typo.mediumBody.copyWith(
+              color: AppColors.primary400,
+              fontWeight: FontWeight.w500,
+              decoration: TextDecoration.underline,
+            ),
+          )),
+        ),
         const Spacer(),
       ],
     );
