d86ad96621db063b088f9507f45e9be524c3a15c
Vehan Hemsara <contact.vehanhemsara@gmail.com>
2025-05-27 11:30:26 +0100

    minor changes

--- lib/core/constants/credentials.dart
+++ lib/core/constants/credentials.dart
@@ -8,9 +8,6 @@ class AppCredentials {
   static const mapBoxAccessToken =
       "pk.eyJ1IjoibGV0ZGVtIiwiYSI6ImNtNXM3NmJpajA5eHQybHBqM3g0MzNrajYifQ.WUCLgcetcoLQKKVKxI6M3g";
 
-  // static const hereAccessKeyId = "WkFR6QoKS8rLDI8dnHLy5Q";
-  // static const hereAccessKeySecret ="iZ0c1xWFMRdbWThSrYotNERWhyyjCydoy2e-A_d8tt6XAJw000Xm8oqr1PyeKwfkZOaqlpA1nR1gr5WtJg2vNQ";
-
   static const hereAccessKeyId = "SzK7h-oZU5WO7BtdhBFf8g";
   static const hereAccessKeySecret =
       "5s2ri60jyt-USlTzU3-qCcepcySRskib7_tPW5SmItNWUmX2nzxvrws_uqETAvxZUmrezf7Yd0fy9sk3TC04pw";
