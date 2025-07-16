074b8bc29c25e8ebf693c33db32f38263ef9de13
GabrielGP <globezno2004@gmail.com>
2025-07-16 13:00:25 +0200

    feat: add keyboard dismiss on tap outside for all forms
    Wrap form screens with GestureDetector to improve UX by allowing users to hide keyboard when tapping outside input fields

--- lib/features/auth/presentation/views/forgot_password/verify_forgot_password_email.view.dart
+++ lib/features/auth/presentation/views/forgot_password/verify_forgot_password_email.view.dart
@@ -69,186 +69,194 @@ class _VerifyForgotPasswordEmailViewState
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: BlocConsumer<AuthBloc, AuthState>(
-          listener: (context, state) {
-            if (state is ValidateResetPasswordSuccess) {
-              NavigatorHelper.to(ResetPasswordView(
-                email: widget.email,
-              ));
-            }
-            if (state is ValidateResetPasswordError) {
-              Toast.showError(state.error);
-            }
-            // TODO: implement listener
-          },
-          builder: (context, state) {
-            return StyledBody(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Spacer(),
-                Column(
-                  children: <Widget>[
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundColor: AppColors.secondary50,
-                      child: Icon(
-                        Iconsax.sms5,
-                        size: 40,
-                        color: AppColors.secondary600,
-                      ),
-                    ),
-                    Dimens.space(3),
-                    Text(
-                      context.l10n.emailSentTitle,
-                      textAlign: TextAlign.center,
-                      style: Typo.heading4.copyWith(color: AppColors.neutral600),
-                    ),
-                    Text(
-                      context.l10n.emailSentDescription,
-                      textAlign: TextAlign.center,
-                      style: Typo.mediumBody.copyWith(color: AppColors.neutral400),
-                    ),
-                    Dimens.space(3),
-                    OTPTextField(
-                      length: 6,
-                      width: MediaQuery.of(context).size.width,
-                      otpFieldStyle: OtpFieldStyle(
-                        enabledBorderColor: AppColors.neutral200,
-                        borderColor: Colors.black.withOpacity(0.2),
-                      ),
-                      fieldWidth: 50,
-                      controller: otpbox,
-                      style: const TextStyle(fontSize: 17),
-                      spaceBetween: 5,
-                      textFieldAlignment: MainAxisAlignment.center,
-                      fieldStyle: FieldStyle.box,
-                      onChanged: (value) {
-                        setState(() {
-                          otp = value;
-                        });
-                      },
-                      onCompleted: (pin) {
-                        setState(() {
-                          otp = pin;
-                        });
-                      },
-                    ),
-                    Dimens.space(3),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: AppColors.tetiary5000,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      padding: EdgeInsets.symmetric(
-                        vertical: Dimens.defaultMargin / 2,
-                        horizontal: Dimens.defaultMargin,
-                      ),
-                      child: Column(
-                        children: [
-                          Center(
-                            child: Text.rich(
-                              TextSpan(
-                                text: context.l10n.mailSentTo,
-                                style: Typo.smallBody.copyWith(),
-                                children: [
-                                  TextSpan(
-                                    text: widget.email,
-                                    style: Typo.smallBody.copyWith(
-                                      decorationColor: AppColors.primary400,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Dimens.space(0.5),
-                          Center(
-                            child: Text.rich(
-                              TextSpan(
-                                text: context.l10n.notYou,
-                                style: Typo.smallBody.copyWith(),
-                                children: [
-                                  TextSpan(
-                                    text: context.l10n.changeEmail,
-                                    style: Typo.smallBody.copyWith(
-                                      color: AppColors.primary400,
-                                      fontWeight: FontWeight.w600,
-                                      decoration: TextDecoration.underline,
-                                      decorationColor: AppColors.primary400,
-                                    ),
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () => NavigatorHelper.pop(),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const Spacer(),
-                PrimaryButton(
-                  onTap: () {
-                    if (otp == null || otp!.length < 6) return;
-                    context.read<AuthBloc>().add(ValidateResetPasswordEvent(
-                          email: widget.email,
-                          code: otp!,
-                        ));
-                  },
-                  isLoading: state is ValidateResetPasswordLoading,
-                  text: context.l10n.proceed,
-                ),
-                Dimens.space(2),
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      if (state is ResendVerificationCodeLoading)
-                        CircularProgressIndicator(
-                          color: AppColors.primary500,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      behavior: HitTestBehavior.opaque,
+      child: Scaffold(
+        body: Center(
+          child: BlocConsumer<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is ValidateResetPasswordSuccess) {
+                NavigatorHelper.to(ResetPasswordView(
+                  email: widget.email,
+                ));
+              }
+              if (state is ValidateResetPasswordError) {
+                Toast.showError(state.error);
+              }
+              // TODO: implement listener
+            },
+            builder: (context, state) {
+              return StyledBody(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Spacer(),
+                  Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundColor: AppColors.secondary50,
+                        child: Icon(
+                          Iconsax.sms5,
+                          size: 40,
+                          color: AppColors.secondary600,
                         ),
-                      Text.rich(
-                        TextSpan(
-                          text: context.l10n.didntGetOtp,
-                          style: Typo.mediumBody.copyWith(),
+                      ),
+                      Dimens.space(3),
+                      Text(
+                        context.l10n.emailSentTitle,
+                        textAlign: TextAlign.center,
+                        style:
+                            Typo.heading4.copyWith(color: AppColors.neutral600),
+                      ),
+                      Text(
+                        context.l10n.emailSentDescription,
+                        textAlign: TextAlign.center,
+                        style: Typo.mediumBody
+                            .copyWith(color: AppColors.neutral400),
+                      ),
+                      Dimens.space(3),
+                      OTPTextField(
+                        length: 6,
+                        width: MediaQuery.of(context).size.width,
+                        otpFieldStyle: OtpFieldStyle(
+                          enabledBorderColor: AppColors.neutral200,
+                          borderColor: Colors.black.withOpacity(0.2),
+                        ),
+                        fieldWidth: 50,
+                        controller: otpbox,
+                        style: const TextStyle(fontSize: 17),
+                        spaceBetween: 5,
+                        textFieldAlignment: MainAxisAlignment.center,
+                        fieldStyle: FieldStyle.box,
+                        onChanged: (value) {
+                          setState(() {
+                            otp = value;
+                          });
+                        },
+                        onCompleted: (pin) {
+                          setState(() {
+                            otp = pin;
+                          });
+                        },
+                      ),
+                      Dimens.space(3),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: AppColors.tetiary5000,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: EdgeInsets.symmetric(
+                          vertical: Dimens.defaultMargin / 2,
+                          horizontal: Dimens.defaultMargin,
+                        ),
+                        child: Column(
                           children: [
-                            TextSpan(
-                              text: _isResendEnabled
-                                  ? context.l10n.tapToResend
-                                  : context.l10n.resendIn(
-                                      _secondsRemaining.toString()),
-                              style: Typo.mediumBody.copyWith(
-                                color: AppColors.primary400,
-                                fontWeight: FontWeight.w600,
-                                decoration: TextDecoration.underline,
-                                decorationColor: AppColors.primary400,
+                            Center(
+                              child: Text.rich(
+                                TextSpan(
+                                  text: context.l10n.mailSentTo,
+                                  style: Typo.smallBody.copyWith(),
+                                  children: [
+                                    TextSpan(
+                                      text: widget.email,
+                                      style: Typo.smallBody.copyWith(
+                                        decorationColor: AppColors.primary400,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              recognizer: TapGestureRecognizer()
-                                ..onTap = () {
-                                  if (_isResendEnabled) {
-                                    context.read<AuthBloc>().add(
-                                        ResendForgotPasswordVerificationCodeEvent(
-                                            email: widget.email));
-                                    _startTimer();
-                                  }
-                                },
+                            ),
+                            Dimens.space(0.5),
+                            Center(
+                              child: Text.rich(
+                                TextSpan(
+                                  text: context.l10n.notYou,
+                                  style: Typo.smallBody.copyWith(),
+                                  children: [
+                                    TextSpan(
+                                      text: context.l10n.changeEmail,
+                                      style: Typo.smallBody.copyWith(
+                                        color: AppColors.primary400,
+                                        fontWeight: FontWeight.w600,
+                                        decoration: TextDecoration.underline,
+                                        decorationColor: AppColors.primary400,
+                                      ),
+                                      recognizer: TapGestureRecognizer()
+                                        ..onTap = () => NavigatorHelper.pop(),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
                           ],
                         ),
                       ),
                     ],
                   ),
-                ),
-                Dimens.space(3),
-              ],
-            );
-          },
+                  const Spacer(),
+                  PrimaryButton(
+                    onTap: () {
+                      if (otp == null || otp!.length < 6) return;
+                      context.read<AuthBloc>().add(ValidateResetPasswordEvent(
+                            email: widget.email,
+                            code: otp!,
+                          ));
+                    },
+                    isLoading: state is ValidateResetPasswordLoading,
+                    text: context.l10n.proceed,
+                  ),
+                  Dimens.space(2),
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        if (state is ResendVerificationCodeLoading)
+                          CircularProgressIndicator(
+                            color: AppColors.primary500,
+                          ),
+                        Text.rich(
+                          TextSpan(
+                            text: context.l10n.didntGetOtp,
+                            style: Typo.mediumBody.copyWith(),
+                            children: [
+                              TextSpan(
+                                text: _isResendEnabled
+                                    ? context.l10n.tapToResend
+                                    : context.l10n
+                                        .resendIn(_secondsRemaining.toString()),
+                                style: Typo.mediumBody.copyWith(
+                                  color: AppColors.primary400,
+                                  fontWeight: FontWeight.w600,
+                                  decoration: TextDecoration.underline,
+                                  decorationColor: AppColors.primary400,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    if (_isResendEnabled) {
+                                      context.read<AuthBloc>().add(
+                                          ResendForgotPasswordVerificationCodeEvent(
+                                              email: widget.email));
+                                      _startTimer();
+                                    }
+                                  },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Dimens.space(3),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
